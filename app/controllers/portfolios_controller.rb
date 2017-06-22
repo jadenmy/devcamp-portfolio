b07c82adeb561f2d251a416e1d0680a56de15690
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:57:27 +0000

    Implemented custom scoped for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,10 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_item = Portfolio.all
+  end
+  
+  def angular
+    @angular_portfolio_item = Portfolio.angular
   end
   
   def new
@@ -51,5 +55,11 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Portfolio was removed.' }
     end
   end
+  
+  def back
+    @portfolio_item = Portfolio.all
+  end
+    
+    
   
 end
